caf5832e2f988f85a21e9aa97f5ae7384b790159
Milena Teixeira <milenateixeira648@gmail.com>
2021-06-21 00:07:52 -0300

    Created POST method used as a test

--- src/server.ts
+++ src/server.ts
@@ -9,5 +9,9 @@ app.get("/test", (request, response) => {
   return response.send("Olá NLW.");
 })
 
+app.post("/test-post", (request, response) => {
+  return response.send("Olá NLW método post.")
+})
+
 //http://localhost:3000
 app.listen(3000, () => console.log("Server is running..."));
