f9ef6bbab68a4ef01e22804ced9c638a89d5637e
Milena Teixeira <milenateixeira648@gmail.com>
2021-06-20 23:50:49 -0300

    Created GET method used as a test

--- src/server.ts
+++ src/server.ts
@@ -3,5 +3,11 @@ import express from 'express';
 //@types/express
 const app = express();
 
+app.get("/test", (request, response) => {
+  //Request: entrada
+  //Response: saída
+  return response.send("Olá NLW.");
+})
+
 //http://localhost:3000
 app.listen(3000, () => console.log("Server is running..."));
