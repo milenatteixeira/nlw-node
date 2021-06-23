32bee048914dcafe6b88924e05422572bd327b80
Milena Teixeira <milenateixeira648@gmail.com>
2021-06-22 22:46:53 -0300

    Added SQLite as database server

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,7 @@
 import express from 'express';
+import "reflect-metadata";
+import "./database";
 
-//@types/express
 const app = express();
 
-app.get("/test", (request, response) => {
-  //Request: entrada
-  //Response: saída
-  return response.send("Olá NLW.");
-})
-
-app.post("/test-post", (request, response) => {
-  return response.send("Olá NLW método post.")
-})
-
-//http://localhost:3000
 app.listen(3000, () => console.log("Server is running..."));
