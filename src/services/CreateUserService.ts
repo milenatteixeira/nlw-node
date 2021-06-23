779de31557e99125bf1815d5b2a2212d9f5b6d2a
Milena Teixeira <milenateixeira648@gmail.com>
2021-06-22 23:48:57 -0300

    Added create user service

---
+++ src/services/CreateUserService.ts
@@ -0,0 +1,32 @@
+import { UsersRepositories } from "../repositories/UsersRepositories";
+
+interface IUserRequest {
+    name: string,
+    email: string,
+    admin?: boolean
+}
+
+class CreateUserService {
+    async execute({ name, email, admin }: IUserRequest) {
+        if (!email) {
+            throw new Error("Email incorrect.");
+        }
+
+        const usersRepository = new UsersRepositories();
+        const userAlreadyExists = await usersRepository.findOne({ email });
+
+        if (userAlreadyExists) {
+            throw new Error("User already exists.");
+        }
+
+        const user = usersRepository.create({
+            name, email, admin
+        });
+
+        await usersRepository.save(user);
+
+        return user;
+    }
+}
+
+export { CreateUserService };
