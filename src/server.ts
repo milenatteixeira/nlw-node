135d5b4120555e45425e26d8bd737614c3c4f623
Milena Teixeira <milenateixeira648@gmail.com>
2021-06-23 22:28:52 -0300

    Added exception tratation on server

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import "express-async-errors";
 import { router } from "./routes";
 import "reflect-metadata";
 import "./database";
@@ -7,4 +8,17 @@ const app = express();
 
 app.use(express.json());
 app.use(router);
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof Error) {
+        return response.status(400).json({
+            error: err.message
+        })
+    }
+
+    return response.status(500).json({
+        status: "error",
+        message: "Internal Server Error"
+    })
+})
+
 app.listen(3000, () => console.log("Server is running..."));
